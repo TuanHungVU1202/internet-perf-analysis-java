ab6437edcd731288ec0626cb6fb2c72e8341942a
TuanHungVU1202 <tuanhung.vu122@gmail.com>
2021-10-12 10:29:23 +0300

    done calculating T1

--- src/main/java/sw/hv/exercise2/E2_Task1.java
+++ src/main/java/sw/hv/exercise2/E2_Task1.java
@@ -1,6 +1,5 @@
 package sw.hv.exercise2;
 
-import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
 import sw.hv.util.GeneralHelper;
 import sw.hv.util.ParseDigResultUtil;
 import sw.hv.util.ParsePingResultUtil;
@@ -13,9 +12,8 @@ import java.util.Scanner;
 
 public class E2_Task1 {
 
-    ArrayList<Double> rttArrLst = new ArrayList<>();
-    int totalNameServerPingRequest = 0;
-    int totalNameServerReceivedPacket = 0;
+
+
 
     public void parseFile () throws Exception {
 //        try {
@@ -27,14 +25,18 @@ public class E2_Task1 {
 //        } catch (Exception e){
 //            throw new Exception(e);
 //        }
-        processPingResult("Test");
+//        processPingResult("Test");
 //        processDigResult ("test");
+        processCurlResult("Test");
     }
 
     // Parse ping result
     private void processPingResult (String filePath) throws Exception {
-        String testPath = "/Users/hungvu/Desktop/E7130/e2/data_43hours/out/reserver_ping";
+        String testPath = "/Users/hungvu/Desktop/E7130/e2/data_43hours/out/nameserver_ping";
         File file = new File(testPath);
+        ArrayList<Double> pingRttArrLst = new ArrayList<>();
+        int totalPingRequest = 0;
+        int totalReceivedPacket = 0;
 
         try{
             Scanner scanner = new Scanner(file);
@@ -44,7 +46,8 @@ public class E2_Task1 {
             while (scanner.hasNextLine()){
                 String line = scanner.nextLine();
 
-                if (line.contains(ParsePingResultUtil.SERVER_HLZ)){
+                //TODO: CHANGE SERVER HERE
+                if (line.contains(ParsePingResultUtil.SERVER_C)){
                     sameServerFlag = true;
                 }
                 // Get all information of successful requests
@@ -54,9 +57,7 @@ public class E2_Task1 {
                     String ip = ParsePingResultUtil.getIp(extractedLst);
                     double rtt = ParsePingResultUtil.getRtt(extractedLst);
                     totalDelay += rtt;
-                    rttArrLst.add(rtt);
-
-//                    System.out.println(ip + " - " + rtt);
+                    pingRttArrLst.add(rtt);
                 }
 
                 // Get number of total requests
@@ -64,19 +65,18 @@ public class E2_Task1 {
                     List<String> totalStatLst = GeneralHelper.extractNumberFromString(line);
                     int transmittedPacket = ParsePingResultUtil.getTransmittedPacket(totalStatLst);
                     int receivedPacket = ParsePingResultUtil.getReceivedPacket(totalStatLst);
-                    totalNameServerPingRequest += transmittedPacket;
-                    totalNameServerReceivedPacket += receivedPacket;
+                    totalPingRequest += transmittedPacket;
+                    totalReceivedPacket += receivedPacket;
                     sameServerFlag = false;
-//                    System.out.println(transmittedPacket + " - " + receivedPacket + " - " + totalRequest);
                 }
             }
             // Sort array
-            if (!rttArrLst.isEmpty()) {
-                Collections.sort(rttArrLst);
-                System.out.println("Median delay= " + calMedianDelay());
-                System.out.println("Mean delay= " + calMeanDelay(totalDelay));
-                System.out.println("Lost ratio= " + calLosttRatio());
-                System.out.println("Delay spread= " + calDelaySpread());
+            if (!pingRttArrLst.isEmpty()) {
+                Collections.sort(pingRttArrLst);
+                System.out.println("PING - Median delay= " + calMedianDelay(pingRttArrLst, totalPingRequest));
+                System.out.println("PING -Mean delay= " + calMeanDelay(totalDelay, totalReceivedPacket));
+                System.out.println("PING -Lost ratio= " + calLosttRatio(totalReceivedPacket, totalPingRequest));
+                System.out.println("PING -Delay spread= " + calDelaySpread(pingRttArrLst, totalReceivedPacket));
             }
         } catch (Exception e){
             throw new Exception(e);
@@ -87,25 +87,94 @@ public class E2_Task1 {
     private void processDigResult (String filePath) throws Exception {
         String testPath = "/Users/hungvu/Desktop/E7130/e2/data_43hours/out/nameserver_dig";
         File file = new File(testPath);
+        ArrayList<Double> queryTimeArrLst = new ArrayList<>();
 
         try{
             Scanner scanner = new Scanner(file);
             double totalDelay = 0;
             boolean sameServerFlag = false;
+            int totalRequest = 0;
+            int totalSucessfulRequest = 0;
 
             while (scanner.hasNextLine()){
                 String line = scanner.nextLine();
 
-                // Get all information of successful requests
-                if (line.contains(ParseDigResultUtil.SERVER_1)){
+                // TODO: CHANGE SERVER HERE
+                if (line.contains(ParseDigResultUtil.SERVER_1) && line.contains(ParseDigResultUtil.DIG_INDICATOR)){
                     sameServerFlag = true;
+                    totalRequest++;
                 }
 
                 if (line.contains(ParseDigResultUtil.QUERY_TIME) && sameServerFlag){
+                   totalSucessfulRequest++;
                    List<String> extractedList = GeneralHelper.extractNumberFromString(line);
-                   System.out.println(extractedList);
+                   double queryTime = ParseDigResultUtil.getQueryTime(extractedList);
+                   queryTimeArrLst.add(queryTime);
+                   totalDelay += queryTime;
                    sameServerFlag = false;
                 }
+            }
+
+            if (!queryTimeArrLst.isEmpty()) {
+                Collections.sort(queryTimeArrLst);
+                System.out.println("DIG - Median delay= " + calMedianDelay(queryTimeArrLst, totalRequest));
+                System.out.println("DIG -Mean delay= " + calMeanDelay(totalDelay, totalSucessfulRequest));
+                System.out.println("DIG -Lost ratio= " + calLosttRatio(totalSucessfulRequest, totalRequest));
+                System.out.println("DIG -Delay spread= " + calDelaySpread(queryTimeArrLst, totalSucessfulRequest));
+            }
+
+        } catch (Exception e){
+            throw new Exception(e);
+        }
+    }
+
+    // Parse curl result
+    private void processCurlResult (String filePath) throws Exception{
+        //TODO: CHANGE SERVER HERE
+        String testPath = "/Users/hungvu/Desktop/E7130/e2/data_43hours/out/iperf_ok1_curl";
+//        String testPath = "/Users/hungvu/Desktop/E7130/e2/data_43hours/out/iperf_sgp1_curl";
+        File file = new File(testPath);
+        ArrayList<Double> totalTimeArrLst = new ArrayList<>();
+        ArrayList<Double> totalDlSpeedArrLst = new ArrayList<>();
+
+        try {
+            Scanner scanner = new Scanner(file);
+            double totalDelay = 0;
+            double totalSpeed = 0;
+            int totalRequest = 0;
+
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+
+                // Delay Time for TASK 1
+                String[] totalDelayArr = line.split(",");
+                double delayTime = Double.parseDouble(totalDelayArr[0]);
+                totalTimeArrLst.add(delayTime);
+                totalDelay += delayTime;
+
+                // Download Speed for TASK 2
+                double downloadSpeed = Double.parseDouble(totalDelayArr[2]);
+                totalDlSpeedArrLst.add(downloadSpeed);
+                totalSpeed +=downloadSpeed;
+
+                totalRequest++;
+            }
+
+            // Calculate delay time for TASK 1
+            if (!totalTimeArrLst.isEmpty()) {
+                Collections.sort(totalTimeArrLst);
+                System.out.println("CURL - Median delay= " + calMedianDelay(totalTimeArrLst, totalRequest));
+                System.out.println("CURL -Mean delay= " + calMeanDelay(totalDelay, totalRequest));
+                System.out.println("CURL -Lost ratio= " + calLosttRatio(totalRequest, totalRequest));
+                System.out.println("CURL -Delay spread= " + calDelaySpread(totalTimeArrLst, totalRequest));
+            }
+
+            // Calculate download speed for TASK 2
+            if (!totalDlSpeedArrLst.isEmpty()) {
+                Collections.sort(totalDlSpeedArrLst);
+                System.out.println("\nCalculating DOWNLOAD Speed");
+                System.out.println("CURL - Median DL= " + calMedianDelay(totalDlSpeedArrLst, totalRequest));
+                System.out.println("CURL - Mean DL= " + calMeanDelay(totalSpeed, totalRequest));
             }
 
         } catch (Exception e){
@@ -114,33 +183,31 @@ public class E2_Task1 {
     }
 
     // median delay WITH lost packets
-    private Double calMedianDelay(){
-        if (totalNameServerPingRequest %2 != 0){
-            return rttArrLst.get(totalNameServerPingRequest/2);
+    private Double calMedianDelay(List<Double> delayArrLst, int totalRequestIncludedLost){
+        if (totalRequestIncludedLost %2 != 0){
+            return delayArrLst.get(totalRequestIncludedLost/2);
         } else {
-            double tmp1 = rttArrLst.get((totalNameServerPingRequest/2)-1);
-            double tmp2 = rttArrLst.get((totalNameServerPingRequest/2));
+            double tmp1 = delayArrLst.get((totalRequestIncludedLost/2)-1);
+            double tmp2 = delayArrLst.get((totalRequestIncludedLost/2));
             return (tmp1 + tmp2)/2;
         }
     }
 
     // mean delay WITHOUT lost packets
-    private double calMeanDelay(Double totalDelay){
-        return totalDelay/totalNameServerReceivedPacket;
+    private double calMeanDelay(Double totalDelay, int totalSuccessfulPacket){
+        return totalDelay/totalSuccessfulPacket;
     }
 
-    private double calLosttRatio(){
-        System.out.println("---re " + totalNameServerReceivedPacket);
-        System.out.println("---total " + totalNameServerPingRequest);
-        return 1 - totalNameServerReceivedPacket/totalNameServerPingRequest;
+    private double calLosttRatio(int totalSuccessfulPacket, int totalRequestIncludedLost){
+//        System.out.println("---re " + totalSuccessfulPacket);
+//        System.out.println("---total " + totalRequestIncludedLost);
+        return 1 - totalSuccessfulPacket/totalRequestIncludedLost;
     }
 
     // Delay spread of 75th and 25th percentiles WITH lost packets
-    private double calDelaySpread(){
-        double percentile_25 = ParsePingResultUtil.percentile(rttArrLst, 25, totalNameServerReceivedPacket);
-        double percentile_75 = ParsePingResultUtil.percentile(rttArrLst, 75, totalNameServerReceivedPacket);
+    private double calDelaySpread(List<Double> delayArrLst, int totalSuccessfulPacket){
+        double percentile_25 = ParsePingResultUtil.percentile(delayArrLst, 25, totalSuccessfulPacket);
+        double percentile_75 = ParsePingResultUtil.percentile(delayArrLst, 75, totalSuccessfulPacket);
         return Math.abs(percentile_75 - percentile_25);
     }
-
-
 }
--- src/main/java/sw/hv/util/ParseDigResultUtil.java
+++ src/main/java/sw/hv/util/ParseDigResultUtil.java
@@ -1,13 +1,17 @@
 package sw.hv.util;
 
+import java.util.List;
+
 public class ParseDigResultUtil {
+    public final static String DIG_INDICATOR = "<<>> DiG";
     public final static String SERVER_1 = "c.nsset.be";
     public final static String SERVER_2 = "y.nsset.be";
     public final static String SERVER_3 = "z.nsset.be";
 
     public final static String QUERY_TIME = "Query time";
+    public final static String TIMESTAMP = "WHEN:";
 
-    public static void parseQuaryTime (){
-
+    public static double getQueryTime (List<String> extractedLst) {
+        return Double.parseDouble(extractedLst.get(0));
     }
 }
